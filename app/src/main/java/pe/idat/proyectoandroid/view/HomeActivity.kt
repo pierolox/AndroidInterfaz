f904733fdaa2affd9afcb8697f0f429ec53822a2
pierolox <pieropiks2018@gmail.com>
2026-04-13 20:06:34 -0500

    menu popup agregado a activity_home para navegacion a otras pantallas (aun no funcional)

--- app/src/main/java/pe/idat/proyectoandroid/view/HomeActivity.kt
+++ app/src/main/java/pe/idat/proyectoandroid/view/HomeActivity.kt
@@ -3,11 +3,13 @@ package pe.idat.proyectoandroid.view
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.PopupMenu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import pe.idat.proyectoandroid.R
 import pe.idat.proyectoandroid.adapter.HabitoAdapter
 import pe.idat.proyectoandroid.data.HabitoData
 import pe.idat.proyectoandroid.databinding.ActivityHomeBinding
@@ -34,6 +36,37 @@ class HomeActivity : AppCompatActivity() {
         binding.fabAgregar.setOnClickListener {
             val intent = Intent(this, CrearHabitoActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.button.setOnClickListener {
+
+            val popup = PopupMenu(this, binding.button)
+            popup.menuInflater.inflate(R.menu.menu_popup, popup.menu)
+
+            popup.setOnMenuItemClickListener { item ->
+
+                when (item.itemId) {
+
+                    R.id.op_metas -> {
+                        startActivity(Intent(this, MetasCompletadasActivity::class.java))
+                        true
+                    }
+
+                    R.id.op_categorias -> {
+                        startActivity(Intent(this, CategoriaActivity::class.java))
+                        true
+                    }
+
+                    R.id.op_notas -> {
+                        startActivity(Intent(this, NotasActivity::class.java))
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+
+            popup.show()
         }
     }
 
